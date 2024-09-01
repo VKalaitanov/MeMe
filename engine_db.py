41b45177fdac8b95d9266efde19499794fc8c3d2
VKalaitanov <kalaytanov93@gmail.com>
2024-09-01 19:23:00 +0300

    debug register user

--- engine_db.py
+++ engine_db.py
@@ -1,4 +1,3 @@
-```
 import os
 from typing import AsyncGenerator
 
@@ -30,4 +29,3 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
-```
